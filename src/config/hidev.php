f34dd8d362b2de03fc21a495630676301086a94c
Andrii Vasyliev <sol@hiqdev.com>
2016-05-27 19:18:39 +0000

    added authManager configuration to hidev

--- src/config/hidev.php
+++ src/config/hidev.php
@@ -7,5 +7,12 @@ return [
                 'class' => \hipanel\rbac\console\RbacController::class,
             ],
         ],
+        ### TODO move to common
+        'authManager' => [
+            'class' => \hipanel\rbac\AuthManager::class,
+            'itemFile' => '@root/rbac/items.php',
+            'ruleFile' => '@root/rbac/rules.php',
+            'assignmentFile' => '@root/rbac/assignments.php',
+        ],
     ],
 ];
