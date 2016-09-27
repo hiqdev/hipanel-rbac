17d17140febdf9720cfa62540bf8b76d7968ba55
Andrii Vasyliev <sol@hiqdev.com>
2016-09-27 14:18:58 +0000

    improved `rbac/show` action

--- src/console/RbacController.php
+++ src/console/RbacController.php
@@ -18,16 +18,20 @@ use yii\rbac\Item;
 /**
  * Class RbacController.
  *
- * Usage: ./hidev rbac/init
+ * Usage: `hidev rbac/init`
  */
 class RbacController extends \yii\console\Controller
 {
     public function actionInit()
     {
         $auth = Yii::$app->get('authManager');
-        $auth->setAssignment('freezer', 'sol');
-
         Initer::init($auth);
+    }
+
+    public function actionReinit()
+    {
+        $auth = Yii::$app->get('authManager');
+        Initer::reinit($auth);
     }
 
     public function actionShow()
@@ -35,14 +39,21 @@ class RbacController extends \yii\console\Controller
         $auth = Yii::$app->get('authManager');
 
         echo "Permissions:\n";
-        foreach ($auth->getItems(Item::TYPE_PERMISSION) as $name => $perm) {
-            echo "\t$perm->name $perm->description\n";
+        foreach ($auth->getPermissions() as $name => $perm) {
+            echo "   $perm->name $perm->description\n";
         }
 
         echo "Roles:\n";
-        foreach ($auth->getItems(Item::TYPE_ROLE) as $name => $role) {
+        foreach ($auth->getRoles() as $name => $role) {
             $children = implode(',', array_keys($auth->getChildren($name)));
-            echo "\t$role->name [$children] $role->description\n";
+            printf("   %-12s %s\n", "$role->name:", $children);
+        }
+
+        echo "Assignments:\n";
+        foreach ($auth->getAllAssignments() as $userId => $roles) {
+            $roles = implode(',', array_keys($roles));
+            printf("   %-12s %s\n", "$userId:", $roles);
         }
     }
+
 }
