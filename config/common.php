2d41c6dbce3c60c7a404ef7008e3c7300f410a0d
Andrii Vasyliev <sol@hiqdev.com>
2019-03-13 11:33:53 +0000

    Used `yii-compat`

--- config/common.php
+++ config/common.php
@@ -8,23 +8,20 @@
  * @copyright Copyright (c) 2016-2019, HiQDev (http://hiqdev.com/)
  */
 
-use yii\helpers\Yii;
+use hiqdev\yii\compat\yii;
 
 $components = [
     'authManager' => [
-        '__class' => \hipanel\rbac\AuthManager::class,
+        yii::classKey() => \hipanel\rbac\AuthManager::class,
     ],
 ];
 
 $singletons = [
-    \yii\rbac\CheckAccessInterface::class => class_exists(Yii::class)
-        ? \yii\di\Reference::to('authManager')
-        : \yii\di\Instance::of('authManager')
-    ,
+    \yii\rbac\CheckAccessInterface::class => yii::referenceTo('authManager'),
     \hipanel\rbac\RbacIniterInterface::class => \hipanel\rbac\Initer::class,
 ];
 
-return !class_exists(Yii::class) ? [
+return yii::is2() ? [
     'components' => $components,
     'container' => [
         'singletons' => $singletons,
